43ef0489f611995eca23bd1ffb67ae5205b0a01a
PriyanshiNayak <83593772+PriyanshiNayak@users.noreply.github.com>
2023-11-30 00:55:43 +0530

    Update streamlit_app.py
    Menu items added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My parents' new healthy diner")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 and Blueberry Oatmeal")
+streamlit.text("Idli and Medu Vada")
+streamlit.text("Hard-boiled free-range egg")

0a3ea66746aac572a322b02770e2fb329043f967
PriyanshiNayak <83593772+PriyanshiNayak@users.noreply.github.com>
2023-12-10 19:50:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.multiselect(list("select * from pc_rivery_db.public.fruit_load_list"))
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+#streamlit.write('The user entered ', fruit_choice)
 
 my_curr.execute = ("insert into fruit_load_list values('from streamlit')")

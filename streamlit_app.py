e69e47bb06be86836137ea5bf377ba996738bf95
PriyanshiNayak <83593772+PriyanshiNayak@users.noreply.github.com>
2023-12-10 19:31:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit load list contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.multiselect(list(fruit_load_list.index))
 
